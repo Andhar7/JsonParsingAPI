0d7dd41c50be4aa63404b938e9a7e289cba81352
Andhar7 <70223638+Andhar7@users.noreply.github.com>
2020-12-09 18:48:10 +0100

    Initial Commit

---
+++ JsonParsingAPI/JsonParsingAPIApp.swift
@@ -0,0 +1,17 @@
+//
+//  JsonParsingAPIApp.swift
+//  JsonParsingAPI
+//
+//  Created by Andrej Kling on 09.12.20.
+//
+
+import SwiftUI
+
+@main
+struct JsonParsingAPIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
